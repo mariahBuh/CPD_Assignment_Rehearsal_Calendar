8d24b431af5335577a370ed0e10881d280b3d7a7
mariahBuh <mariah.buhagiar.g52824@mcast.edu.mt>
2025-03-21 14:45:50 +0100

    Implemented Home Screen Code and Updated the main.dart code

--- lib/screens/add_rehearsal.dart
+++ lib/screens/add_rehearsal.dart
@@ -1,1 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'home_Screen.dart'; 
 
--- lib/screens/home_Screen.dart
+++ lib/screens/home_Screen.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rehearsal Calendar',style: const TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold
+        ),),
+        backgroundColor: const Color(0xFF165E7F),centerTitle: true,
+
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'No Rehearsals Added.',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(150, 48),
+                  backgroundColor: const Color(0xFF165E7F),
+                ),
+                child: const Text(
+                  'Add Rehearsal',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
